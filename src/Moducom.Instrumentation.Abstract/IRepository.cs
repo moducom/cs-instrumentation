f2b9e5d4f46994dfb5338643b66d92715e82954f
Malachi Burke <malachi.burke@gmail.com>
2017-11-08 08:43:11 -0800

    Adding unit test and more boilerplate

---
+++ src/Moducom.Instrumentation.Abstract/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Moducom.Instrumentation.Abstract
+{
+    public interface IRepository
+    {
+        INode RootNode { get; }
+
+        INode this[string path] { get; }
+    }
+}
