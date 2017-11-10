929d3c6063b9b4703fd5229ad8ba96dc9dce05b0
Malachi Burke <malachi.burke@gmail.com>
2017-11-10 14:18:28 -0800

    Adding untested slightly-more-strongly-typed IMetricNode code

--- src/Moducom.Instrumentation.Abstract/IRepository.cs
+++ src/Moducom.Instrumentation.Abstract/IRepository.cs
@@ -12,6 +12,76 @@ namespace Moducom.Instrumentation.Abstract
         INode this[string path] { get; }
     }
 
+    namespace Experimental
+    {
+        internal class MetricNodeWrapperExperimental<T> : IMetricNode<T>
+            where T : IMetricBase
+        {
+            readonly INode node;
+
+            protected MetricNodeWrapperExperimental(INode node)
+            {
+                this.node = node;
+            }
+
+            public void AddMetric(IMetricBase metric)
+            {
+                node.AddMetric(metric);
+            }
+
+            public T AddMetric<T>(string key = null) where T : IMetricBase
+            {
+                return node.AddMetric<T>(key);
+            }
+
+            public IEnumerable<IMetricBase> GetMetrics(object labels = null)
+            {
+                return node.GetMetrics(labels);
+            }
+
+            /// <summary>
+            /// FIX: labels could very well result in multiple matches rather than single one.  Unsure how to
+            /// handle this right now
+            /// </summary>
+            /// <param name="labels"></param>
+            /// <returns></returns>
+            /// <remarks>
+            /// Probably this Labels would act ass the Add metric too, if you request a non-present label
+            /// probably we want to create it ad-hoc
+            /// </remarks>
+            public T Labels(object labels)
+            {
+                // FIX: Undefined behavior if *NOT* an ICounter, but probably need
+                // to define the behavior
+                var metrics = node.GetMetrics(labels).OfType<T>();
+
+                // If no metrics match on the labels, then set out to create a new one
+                if (!metrics.Any())
+                {
+                    // FIX: Likely get this from a factory
+                    /*
+                    ICounter counter = null;
+
+                    node.AddMetric(counter); */
+                    T counter = node.AddMetric<T>();
+
+                    counter.SetLabels(labels);
+
+                    return counter;
+                }
+                else
+                    return metrics.Single();
+            }
+        }
+
+        internal class CounterNodeWrapperExperimental : 
+            MetricNodeWrapperExperimental<ICounter>, 
+            ICounterNode
+        {
+            internal CounterNodeWrapperExperimental(INode node) : base(node) { }
+        }
+    }
+
     public static class IRepositoryExtensions
     {
         /// <summary>
@@ -20,14 +90,17 @@ namespace Moducom.Instrumentation.Abstract
         /// <param name="path"></param>
         /// <param name="labels"></param>
         /// <returns></returns>
+        /// <remarks>
+        /// FIX: Undefined behavior if labels match multiple, but shouldn't be undefined
+        /// </remarks>
         public static ICounter GetCounterExperimental(this IRepository repository, string path, object labels = null)
         {
-            return (ICounter) repository[path].GetMetric(null);
+            return repository[path].GetMetrics(labels).OfType<ICounter>().Single();
         }
 
         public static Experimental.ICounterNode GetCounterExperimental(this IRepository repository, string path)
         {
-            return (Experimental.ICounterNode)repository[path];
+            return new Experimental.CounterNodeWrapperExperimental(repository[path]);
         }
     }
 }
