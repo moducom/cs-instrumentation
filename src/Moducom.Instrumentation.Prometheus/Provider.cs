2344ccd79a676aedb8fd21739a99fedcb979b194
Malachi Burke <malachi.burke@gmail.com>
2017-11-15 11:45:20 -0800

    Some foundational test code.  Nothing more

---
+++ src/Moducom.Instrumentation.Prometheus/Provider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using global::Prometheus.Client.Collectors;
+using global::Prometheus.Client;
+
+namespace Moducom.Instrumentation.Prometheus
+{
+    public class Provider
+    {
+        public Provider()
+        {
+            var instance = CollectorRegistry.Instance;
+        }
+    }
+}
---
+++ src/Moducom.Instrumentation.Test/PrometheusTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moducom.Instrumentation.Abstract;
+using Moducom.Instrumentation.Abstract.Experimental;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using PRO = Moducom.Instrumentation.Prometheus;
+using Prometheus.Client;
+using Prometheus.Client.Collectors;
+
+namespace Moducom.Instrumentation.Test
+{
+    [TestClass]
+    public class PrometheusTests
+    {
+        [TestMethod]
+        public void PrometheusBaseTest()
+        {
+            var cr = CollectorRegistry.Instance;
+
+            var counter = Metrics.CreateCounter("myCounter", "Description of my counter", "allowed_label");
+
+            counter.Labels("5").Inc();
+
+            // this won't work, only 'allowed_label' defined.  
+            //counter.Labels("4", "test").Inc();
+
+            // interestingly this does work, so null labels permitted
+            counter.Inc();
+
+            // dummy code so far
+            var p = new PRO.Provider();
+        }
+    }
+}
