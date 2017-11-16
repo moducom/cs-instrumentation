e43a96c72ee66957b6c8afc4b5620f496c9b5c93
Malachi Burke <malachi.burke@gmail.com>
2017-11-16 09:10:49 -0800

    Adding experimental C++ DLL and unit test stubs
    Interestingly, unit test doesn't easily see C++ DLL

---
+++ src/Moducom.Instrumentation.CppExperimental/Moducom.Instrumentation.CppExperimental.h
@@ -0,0 +1,13 @@
+// Moducom.Instrumentation.CppExperimental.h
+
+#pragma once
+
+using namespace System;
+
+namespace ModucomInstrumentationCppExperimental {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
