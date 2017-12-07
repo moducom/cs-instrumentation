a7dce4e54501aeb674107e26b2a91dbdbec814a9
Malachi Burke <malachi.burke@gmail.com>
2017-12-07 14:07:24 -0800

    Adding boilerplate-ish exporter test

---
+++ src/Moducom.Instrumentation.Prometheus.TestServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using Prometheus.Client.MetricServer;
+using System;
+
+namespace Moducom.Instrumentation.Prometheus.TestServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMetricServer metricServer = new MetricServer("localhost", 9100);
+
+            metricServer.Start();
+
+            Console.WriteLine("Metric server running");
+            Console.ReadLine();
+
+            metricServer.Stop();
+        }
+    }
+}
