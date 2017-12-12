15f80f0271fdfc9558ee05a37831639db0cff2e1
Malachi Burke <malachi.burke@gmail.com>
2017-12-11 16:55:06 -0800

    Prep code for proof of concept
    Of our minimal IoC code being actually exported and picked up

--- src/Moducom.Instrumentation.Prometheus.TestServer/Program.cs
+++ src/Moducom.Instrumentation.Prometheus.TestServer/Program.cs
@@ -5,11 +5,24 @@ namespace Moducom.Instrumentation.Prometheus.TestServer
 {
     class Program
     {
+        static void NativeExporter()
+        {
+
+        }
+
+
+        static void WrappedExporter()
+        {
+            // TODO: Utilize what minimal IoC tricks we have here
+        }
+
         static void Main(string[] args)
         {
             IMetricServer metricServer = new MetricServer("localhost", 9100);
 
             metricServer.Start();
+
+            WrappedExporter();
 
             Console.WriteLine("Metric server running");
             Console.ReadLine();
