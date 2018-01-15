bc6c6d9f460534ffa4cea9b5a8323dcc7225dc8f
Malachi Burke <malachi.burke@gmail.com>
2018-01-15 14:31:25 -0800

    Initial chunk for aggregate repository, but not active

---
+++ src/Moducom.Instrumentation.Experimental/AggregateRepository.cs
@@ -0,0 +1,64 @@
+﻿using Moducom.Instrumentation.Abstract;
+using Moducom.Instrumentation.Abstract.Experimental;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moducom.Instrumentation.Experimental
+{
+    class AggregateRepository : IRepository
+    {
+        LinkedList<IRepository> repositories = new LinkedList<IRepository>();
+
+        class Metric : ILabelsProvider, IValueGetter
+        {
+            public IEnumerable<string> Labels => throw new NotImplementedException();
+
+            public bool GetLabelValue(string label, out object value)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        class Counter : Metric, ICounter
+        {
+            public double Value => throw new NotImplementedException();
+
+            public void Increment(double byAmount)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class Node : INode
+        {
+            public IEnumerable<INode> Children => throw new NotImplementedException();
+
+            public IEnumerable<IMetricBase> Metrics => throw new NotImplementedException();
+
+            public string Name => throw new NotImplementedException();
+
+            public INode GetChild(string key)
+            {
+                throw new NotImplementedException();
+            }
+
+            public T GetMetric<T>(object labels = null) where T : ILabelsProvider, IValueGetter
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerable<IMetricBase> GetMetrics(object labels)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public INode this[string key] => throw new NotImplementedException();
+
+        public INode RootNode => throw new NotImplementedException();
+    }
+}
